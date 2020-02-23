d9eca92e4af8cb139e1dbf609c75cdc836c36367
General Assembly Laptop <generalassemblylaptop@Generals-MacBook-Pro.local>
2020-02-23 15:32:08 -0500

    more game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,1 +1,15 @@
 console.log("Up and running!");
+cards = ["queen","queen","queen","king"];
+inPlay = [];
+cardOne = cards[0];
+inPlay.push(cardOne);
+cardTwo = cards[2];
+inPlay.push(cardTwo);
+if (inPlay.length === 2){
+	if(inPlay[0]===inPlay[1]){
+		alert("You found a match!");
+	}
+	else{
+		alert("Sorry, try again...");
+	}
+}
