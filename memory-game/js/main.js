bd772b1154c10675ef0d3f81121c6aa1ff5b796d
General Assembly Laptop <generalassemblylaptop@Generals-MBP.fios-router.home>
2020-02-24 23:58:31 -0500

    created game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -23,6 +23,15 @@ let cards = [
 ];
 let inPlay = [];
 
+function createBoard(){
+	for(let i = 0; i < cards.length;i++){
+		let newCard = document.createElement('img');
+		newCard.setAttribute('src','images/back.png');
+		newCard.setAttribute('data-id',i);
+		newCard.addEventListener('click',flipCard());
+		document.getElementById('game-board').appendChild(newCard);
+	}
+}
 function checkMatch(){
 	if(inPlay[0]===inPlay[1]){
 		alert("You found a match!");
@@ -32,14 +41,15 @@ function checkMatch(){
 	}
 }
 
-function flipCard(cardId){
+function flipCard(){
+	let cardId = this.getAttribute('data-id');
 	inPlay.push(cards[cardId].rank);
+	this.setAttribute('src',cards[cardId].cardImage);
 	if (inPlay.length === 2){
 		chechMatch();
 	}
 }
 
-flipCard(0);
-flipCard(1);
+createBoard();
 
 
